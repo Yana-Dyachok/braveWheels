0c3abd60163b32515fd4e05b2ab5d39d5658f313
Alexander <sasacihun@gmail.com>
2023-11-22 13:09:41 +0200

    API, Added Postgres, Hide sensitive data, Code formatting

--- backend/online_store/api/urls.py
+++ backend/online_store/api/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
-
 from . import views
 
+from rest_framework import routers
+from .views import BicycleAPI
+from django.urls import include
+
+router = routers.DefaultRouter()
+router.register(r'bicycle', BicycleAPI)
+
 urlpatterns = [
-    path("", views.index, name="index")
+    # all URL's
+    # path("", views.index),
+    path("", include(router.urls)),
 ]
