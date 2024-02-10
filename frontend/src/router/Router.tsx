3da88615655d3e0738ce397681a9cc815781b199
Yana-Dyachok <annetart1994@gmail.com>
2024-02-10 14:53:31 +0200

    feat: add  error page

--- frontend/src/router/Router.tsx
+++ frontend/src/router/Router.tsx
@@ -1,13 +1,15 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from 'react-router-dom';
 
-import { CartPage, HomePage } from "../pages";
+import { CartPage, HomePage} from '../pages';
+import { ErrorPage } from '../pages/error/ErrorPage';
 
 export const Router = () => {
-  return (
-    <Routes>
-      <Route path="/home" element={<HomePage />} />
-      <Route path="/" element={<Navigate to="/home" replace />} />
-      <Route path="/cart" element={<CartPage />} />
-    </Routes>
-  );
-}
+    return (
+        <Routes>
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/" element={<Navigate to="/home" replace />} />
+            <Route path="/cart" element={<CartPage />} />
+            <Route path="/error" element={<ErrorPage />} />
+        </Routes>
+    );
+};
